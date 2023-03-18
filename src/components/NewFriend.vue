337a0f16b9ad63487958099128f27fbbf7dc5846
pietroscandola <pietro.scandolavr@gmail.com>
2023-03-18 11:08:38 +0100

    first commit

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>E-mail</label>
+      <input type="email" v-model="enteredEmail" />
+    </div>
+    <div>
+      <button @click="submiData">Add Contact</button>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    submiData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+    },
+  },
+};
+</script>
